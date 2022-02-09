e30ea5a9e65fb7cdff696d3c39366c93401b6522
Daniel Oliveira <danielcoliv98@gmail.com>
2022-02-09 15:10:24 -0300

    Layout de participantes com funçao de exibir

--- script/script.js
+++ script/script.js
@@ -12,6 +12,7 @@ function selectTypeMessage(element){
     deselect('message-option');
     element.classList.add("selected");
 }
+
 function deselect (className){
 
     const elementSelected = document.querySelector("."+className+".selected")
@@ -19,4 +20,11 @@ function deselect (className){
     if(elementSelected !== null){
         elementSelected.classList.remove("selected")
     }
+}
+
+function navInteract(){
+    const blur = document.querySelector(".blur")
+    blur.classList.toggle("hidden")
+    const nav = document.querySelector("nav");
+    nav.classList.toggle("show") 
 }
