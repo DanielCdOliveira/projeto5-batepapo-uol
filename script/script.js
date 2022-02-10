22f6275e17fb79ea3f3a43c6f7727647245d6e31
Daniel Oliveira <danielcoliv98@gmail.com>
2022-02-10 12:10:18 -0300

    Manter conexão e buscar mensagens

--- script/script.js
+++ script/script.js
@@ -1,3 +1,10 @@
+const messagesList = document.querySelector(".messages")
+console.log(messagesList);
+
+
+
+
+
 let userName = {
     name: ""
 };
@@ -5,10 +12,9 @@ let userName = {
 function login() {
     userName.name = document.querySelector(".username").value
     const promise = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants", userName)  
-    console.log(promise)  
+
     promise.then(loginSucess)
     promise.catch(loginFailure)
-
 
 
 }
@@ -16,11 +22,63 @@ function login() {
 function loginSucess() {
     let loginScreen = document.querySelector(".login-screen")
     loginScreen.classList.add("hidden")
-
+    setInterval(sendStatus, 5000)
+    searchMessages()
 }
 function loginFailure(){
     alert("Nome de usuario ja existente")
 }
+
+
+function sendStatus (){
+    axios.post("https://mock-api.driven.com.br/api/v4/uol/status",userName)
+}
+function searchMessages(){
+    const promise = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages")
+    promise.then(insertMessages)
+
+
+
+}
+
+
+function insertMessages(response){
+    response.data.forEach(element => {
+       compareStatus(element);
+    });
+}
+
+function compareStatus (element){
+    if(element.type === "status"){
+        messagesList.innerHTML += `<li class="message status">
+        <span>${element.time}</span>
+        <p><strong>${element.from}</strong> ${element.text}</p>
+    </li>`
+    }else if(element.type === "message"){
+        messagesList.innerHTML += `<li class="message all">
+            <span>${element.time}</span>
+            <p><strong>${element.from}</strong> para <strong>${element.to}</strong>: ${element.text}</p>
+        </li>`
+    }else if(element.type === "private_message"){
+        if(element.from == userName){
+            messagesList.innerHTML +=`<li class="message private">
+            <span>${element.time}</span>
+            <p><strong>${element.from}</strong> reservadamente para <strong>${element.to}</strong>: ${element.text}</p>
+        </li>`
+        }
+    }
+    
+}
+
+
+
+
+
+messagesList.innerHTML += `<li class="message status">
+        <span>${element.time}</span>
+        <p><strong>${element.from}</strong> ${element.text}</p>
+    </li>`
+
 
 // ENVIAR MENSAGEM
 
