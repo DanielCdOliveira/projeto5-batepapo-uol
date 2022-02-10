fde9258215b04e5ff9022edf3d0dc5745a4d5545
Daniel Oliveira <danielcoliv98@gmail.com>
2022-02-10 09:41:35 -0300

    POST username

--- script/script.js
+++ script/script.js
@@ -1,21 +1,53 @@
-
+let userName = {
+    name: ""
+};
 
 function login() {
-    let userName = document.querySelector(".username").value
-    let loginScreen = document.querySelector(".login-screen")
-    loginScreen.classList.add("hidden")
+    userName.name = document.querySelector(".username").value
+    const promise = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants", userName)  
+    console.log(promise)  
+    promise.then(loginSucess)
+    promise.catch(loginFailure)
+
+
+
 }
 
+function loginSucess() {
+    let loginScreen = document.querySelector(".login-screen")
+    loginScreen.classList.add("hidden")
+
+}
+function loginFailure(){
+    alert("Nome de usuario ja existente")
+}
+
+// ENVIAR MENSAGEM
+
+function sendMessage() {
+
+    
+    let type = document.querySelector('.message-type.selected').querySelector("span").innerText
+    if(type == "Público"){
+        type = "message";
+    }else{
+        type = "private_message"
+    }
+
+    let message = {
+        from: userName.name,
+        to: document.querySelector('.contact.selected').querySelector("span").innerText,
+        text: document.querySelector("#message-text").value,
+        type: type
+
+    }
+    console.log(message)
 
 
 
 
 
-
-
-
-
-
+}
 
 // FUNÇOES DO MENU LATERAL
 
@@ -25,14 +57,13 @@ function selectContact(element) {
 }
 
 function selectTypeMessage(element) {
-    deselect('message-option');
+    deselect('message-type');
     element.classList.add("selected");
 }
 
 function deselect(className) {
 
-    const elementSelected = document.querySelector("." + className + ".selected")
-
+    const elementSelected = document.querySelector(`.${className}.selected`)
     if (elementSelected !== null) {
         elementSelected.classList.remove("selected")
     }
